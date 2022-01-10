086e609328cd3c21154286d3b893b8c5867860b3
Sushil Kumar <sushilkalwa@yahoo.co.in>
2022-01-09 22:10:36 -0800

    token authorization set up

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -7,6 +7,7 @@ using HotelListing.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using Serilog.Events;
 using System.Configuration;
@@ -43,7 +44,35 @@ builder.Services.AddScoped<IAuthManager, AuthManager>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = $"JWT Authorization header using the Bearer scheme.",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference=new OpenApiReference
+				{
+					Type=ReferenceType.SecurityScheme,
+					Id="Bearer"
+				},
+				Scheme="0auth2",
+				Name="Bearer",
+				In=ParameterLocation.Header
+			},
+			new List<string>()
+		}
+	});
+	//c.SwaggerDoc("V1", new OpenApiInfo { Title = "HotelListing", Version = "V1" });
+});
 
 
 var app = builder.Build();
